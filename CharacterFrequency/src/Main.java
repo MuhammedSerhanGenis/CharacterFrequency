01d12285206267b162d6a343d45467a42ba6fc9d
MuhammedSerhanGenis <87130649+MuhammedSerhanGenis@users.noreply.github.com>
2022-08-14 18:04:12 +0300

    Add files via upload

---
+++ CharacterFrequency/src/Main.java
@@ -0,0 +1,50 @@
+/**
+  *Finding number of repetitions of each character in a sentence using Map Interface.
+  * 
+  *Muhammed Serhan Genis
+  *13.08.2022
+  *
+*/
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+
+public class Main {
+    
+    
+    
+    public static void main(String args[]){
+        
+        Map<Character,Integer> charFrequency=new TreeMap<Character,Integer>();
+        
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Please enter a sentence : ");
+        String sentence= scanner.nextLine();
+        System.out.println();
+        for(int i=0;i<sentence.length();i++){
+            
+            char c= sentence.charAt(i);
+            
+            if(charFrequency.containsKey(c)){
+                charFrequency.put(c, charFrequency.get(c)+1);
+            }
+            else{
+                charFrequency.put(c, 1);
+            }
+            
+        }
+        
+        for(Character key: charFrequency.keySet()){
+            
+            System.out.println("Character "+key+" : "+charFrequency.get(key)+" times.");
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+}
